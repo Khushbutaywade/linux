3bb5434fd5358ab7108d84ffa3412c06957e7e89
Khushbutaywade <86351764+Khushbutaywade@users.noreply.github.com>
2024-01-19 12:55:31 +0530

    Add files via upload

---
+++ hello-world/helloworld.groovy
@@ -0,0 +1,9 @@
+import java.util.Date
+
+class HelloWorld {
+    static void main(String[] args) {
+        println "Hello, World!"
+        Date currentDate = new Date()
+        println "Current date: $currentDate"
+    }
+}
